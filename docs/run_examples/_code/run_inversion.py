5389c43d69b69634b3ef89a631606d42fb529955
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2020-07-05 01:09:14 +0200

    Convert single run example to how it's supposed to work

--- docs/run_examples/_code/run_inversion.py
+++ docs/run_examples/_code/run_inversion.py
@@ -3,79 +3,98 @@ import logging
 
 # Libs
 import geopandas as gpd
+import dask
+from dask.distributed import Client
 
 # Locals
-import oggm.cfg as cfg
-from oggm import utils, workflow, tasks
+from oggm import OGGM, tasks, utils
 
 # For timing the run
 import time
-start = time.time()
 
-# Module logger
-log = logging.getLogger(__name__)
+if __name__ == "__main__":
+    start = time.time()
 
-# Initialize OGGM and set up the default run parameters
-cfg.initialize(logging_level='WORKFLOW')
-rgi_version = '61'
-rgi_region = '11'  # Region Central Europe
+    # Module logger
+    log = logging.getLogger(__name__)
 
-# Local working directory (where OGGM will write its output)
-WORKING_DIR = utils.gettempdir('OGGM_Inversion')
-utils.mkdir(WORKING_DIR, reset=True)
-cfg.PATHS['working_dir'] = WORKING_DIR
+    # Dask Client
+    client = Client()
 
-# RGI file
-path = utils.get_rgi_region_file(rgi_region, version=rgi_version)
-rgidf = gpd.read_file(path)
+    # Initialize OGGM and set up the default run parameters
+    oggm = OGGM()
+    oggm.initialize(logging_level='WORKFLOW')
+    rgi_version = '61'
+    rgi_region = '11'  # Region Central Europe
 
-# Select the glaciers in the Pyrenees
-rgidf = rgidf.loc[rgidf['O2Region'] == '2']
+    # Local working directory (where OGGM will write its output)
+    WORKING_DIR = utils.gettempdir('OGGM_Inversion')
+    utils.mkdir(WORKING_DIR, reset=True)
+    oggm.PATHS['working_dir'] = WORKING_DIR
 
-# Sort for more efficient parallel computing
-rgidf = rgidf.sort_values('Area', ascending=False)
+    # RGI file
+    path = oggm.utils.get_rgi_region_file(rgi_region, version=rgi_version)
+    rgidf = dask.delayed(gpd.read_file)(path)
 
-log.workflow('Starting OGGM inversion run')
-log.workflow('Number of glaciers: {}'.format(len(rgidf)))
+    @dask.delayed
+    def filter_rgidf(rgidf):
+        # Select the glaciers in the Pyrenees
+        rgidf = rgidf.loc[rgidf['O2Region'] == '2']
 
-# Go - get the pre-processed glacier directories
-# We start at level 3, because we need all data for the inversion
-gdirs = workflow.init_glacier_directories(rgidf,
+        # Sort for more efficient parallel computing
+        rgidf = rgidf.sort_values('Area', ascending=False)
+
+        log.workflow('Starting OGGM inversion run')
+        log.workflow('Number of glaciers: {}'.format(len(rgidf)))
+
+    rgidf = filter_rgidf(rgidf)
+
+    # Go - get the pre-processed glacier directories
+    # We start at level 3, because we need all data for the inversion
+    gdirs = oggm.init_glacier_directories(rgidf,
                                           from_prepro_level=3,
                                           prepro_border=10)
 
-# Default parameters
-# Deformation: from Cuffey and Patterson 2010
-glen_a = 2.4e-24
-# Sliding: from Oerlemans 1997
-fs = 5.7e-20
+    # Default parameters
+    # Deformation: from Cuffey and Patterson 2010
+    glen_a = 2.4e-24
+    # Sliding: from Oerlemans 1997
+    fs = 5.7e-20
 
-# Correction factors
-factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-factors += [1.1, 1.2, 1.3, 1.5, 1.7, 2, 2.5, 3, 4, 5]
-factors += [6, 7, 8, 9, 10]
+    # Correction factors
+    factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    factors += [1.1, 1.2, 1.3, 1.5, 1.7, 2, 2.5, 3, 4, 5]
+    factors += [6, 7, 8, 9, 10]
 
-# Run the inversions tasks with the given factors
-for f in factors:
-    # Without sliding
-    suf = '_{:03d}_without_fs'.format(int(f * 10))
-    workflow.execute_entity_task(tasks.mass_conservation_inversion, gdirs,
-                                 glen_a=glen_a*f, fs=0)
-    workflow.execute_entity_task(tasks.filter_inversion_output, gdirs)
-    # Store the results of the inversion only
-    utils.compile_glacier_statistics(gdirs, filesuffix=suf,
-                                     inversion_only=True)
+    results = []
 
-    # With sliding
-    suf = '_{:03d}_with_fs'.format(int(f * 10))
-    workflow.execute_entity_task(tasks.mass_conservation_inversion, gdirs,
-                                 glen_a=glen_a*f, fs=fs)
-    workflow.execute_entity_task(tasks.filter_inversion_output, gdirs)
-    # Store the results of the inversion only
-    utils.compile_glacier_statistics(gdirs, filesuffix=suf,
-                                     inversion_only=True)
+    # Run the inversions tasks with the given factors
+    for f in factors:
+        # Without sliding
+        suf = '_{:03d}_without_fs'.format(int(f * 10))
+        output_gdirs = oggm.execute_entity_task([
+                tasks.mass_conservation_inversion(glen_a=glen_a*f, fs=0),
+                tasks.filter_inversion_output
+            ], gdirs)
+        # Store the results of the inversion only
+        res = utils.compile_glacier_statistics(output_gdirs, filesuffix=suf, inversion_only=True)
 
-# Log
-m, s = divmod(time.time() - start, 60)
-h, m = divmod(m, 60)
-log.workflow('OGGM is done! Time needed: %d:%02d:%02d' % (h, m, s))
+        # With sliding
+        suf = '_{:03d}_with_fs'.format(int(f * 10))
+        output_gdirs = oggm.execute_entity_task([
+                tasks.mass_conservation_inversion(glen_a=glen_a*f, fs=fs),
+                tasks.filter_inversion_output
+            ], gdirs)
+        # Store the results of the inversion only
+        res = utils.compile_glacier_statistics(output_gdirs, filesuffix=suf, inversion_only=True)
+        results.append(res)
+
+    results = client.compute(results)
+    results = client.gather(results)
+    
+    print(repr(results))
+
+    # Log
+    m, s = divmod(time.time() - start, 60)
+    h, m = divmod(m, 60)
+    log.workflow('OGGM is done! Time needed: %d:%02d:%02d' % (h, m, s))
